259e533165cdd1280f956bb29dc3207eafd7832b
unmilan17 <unmilan17@gmail.com>
2023-10-31 17:01:38 +0530

    changes to admin and srtucture

--- admin.h
+++ admin.h
@@ -22,7 +22,7 @@ void clear_leftover()
 }
 
 // Function to delete a record by unique column value
-void deleteRecord(const char *filename, const char *uniqueValue)
+void delDriver(const char *filename, const char *nameToDelete)
 {
     FILE *inputFile = fopen(filename, "r");
     FILE *tempFile = fopen("temp.csv", "w");
@@ -33,20 +33,19 @@ void deleteRecord(const char *filename, const char *uniqueValue)
         exit(EXIT_FAILURE);
     }
 
-    char line[1024]; // Adjust this buffer size based on your CSV file's expected maximum line length
+    char driverName[100];    // Adjust buffer size based on the expected driver name length
+    char vehicleNumber[100]; // Adjust buffer size based on the expected vehicle number length
     int found = 0;
 
-    while (fgets(line, sizeof(line), inputFile))
+    while (fscanf(inputFile, " %99[^|]|%99[^|\n]\n", driverName, vehicleNumber) != EOF)
     {
-        char *token = strtok(line, ",");
-
-        if (strcmp(token, uniqueValue) == 0)
+        if (strcmp(driverName, nameToDelete) == 0)
         {
             found = 1;
             continue; // Skip this line (record to be deleted)
         }
 
-        fputs(line, tempFile);
+        fprintf(tempFile, "%s|%s\n", driverName, vehicleNumber); // Write the line back to the temporary file
     }
 
     fclose(inputFile);
@@ -66,11 +65,11 @@ void deleteRecord(const char *filename, const char *uniqueValue)
 
     if (found)
     {
-        printf("Record with unique value '%s' deleted successfully.\n", uniqueValue);
+        printf("Record for driver '%s' deleted successfully.\n", nameToDelete);
     }
     else
     {
-        printf("Record with unique value '%s' not found.\n", uniqueValue);
+        printf("Record for driver '%s' not found.\n", nameToDelete);
     }
 }
 
@@ -366,7 +365,7 @@ void admin()
                     fclose(fp);
 
                     clear_leftover();
-                    printf("Enter vehicle number of the vehicle to be deleted: \n\t\t\t\t");
+                    printf("Enter vehicle number of the vehicle to be deleted: ");
                     fgets(delVehicle.vehicleNumber, 20, stdin);
                     remove_endline(delVehicle.vehicleNumber);
                     int chck = 0;
@@ -386,12 +385,52 @@ void admin()
 
                     else
                     {
-                        deleteRecord("vehicle_details.csv", delVehicle.vehicleNumber);
                         printf("\t\t\t\tRecord has been deleted");
                     }
                 }
                 break;
+            case 2:
+                fp = fopen("order_details.csv", "r");
 
+                if (fp == NULL)
+                {
+                    printf("\t\t\t\tError opening the file.\n");
+                }
+
+                else
+                {
+                    printf("Customer name\tPick up location\tDelivery destination\n");
+
+                    // Read and print each line from the CSV file
+                    while (fscanf(fp, "%99[^|]|%99[^|]|%99[^\n]\n", read_o.cus.name, read_o.pick_loc, read_o.del_dest) != EOF)
+                    {
+                        printf("%-10s\t%-19s\t%s\n", read_o.cus.name, read_o.pick_loc, read_o.del_dest);
+                    }
+                    fclose(fp);
+
+                    clear_leftover();
+                    printf("Enter driver name to be deleted: ");
+                    fgets(del_d.name, 100, stdin);
+                    remove_endline(del_d.name);
+                    int chck = 0;
+                    fp = fopen("driver_details.csv", "r");
+                    while (fscanf(fp, "%99[^|]|%19[^\n]\n", read_d.name, read_d.curr_veh.vehicleNumber) != EOF)
+                    {
+                        if (strcmp(del_d.name, read_d.name))
+                        {
+                            chck = 1;
+                            break;
+                        }
+                    }
+                    if (chck == 0)
+                    {
+                        printf("\t\t\t\tDriver not in list\n");
+                    }
+                    else
+                    {
+                        delDriver("driver_details", del_d.name);
+                    }
+                }
             default:
                 break;
             }
